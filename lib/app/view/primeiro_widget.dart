eae738a7d542349317fab4975cfe20ad229353d0
ricardo <valverde.ricardojr@gmail.com>
2022-02-28 23:01:46 -0300

    update

---
+++ lib/app/view/primeiro_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrimeiroWidget extends StatefulWidget {
+  const PrimeiroWidget({Key? key}) : super(key: key);
+
+  @override
+  State<PrimeiroWidget> createState() => _PrimeiroWidgetState();
+}
+
+class _PrimeiroWidgetState extends State<PrimeiroWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
